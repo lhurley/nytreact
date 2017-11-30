0f5a727ce02d7edde5fba7c43cb0a3e07d1e6441
Lizzie Hurley <l.hurley58@gmail.com>
2017-11-29 19:50:56 -0600

    update

--- nytreact/client/src/components/article.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Article extends Component {
-  render() {
-    return (
-      <div></div>
-    );
-  }
-}
--- nytreact/routes/index.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = function(express) {
-  let router = express.Router();
-  router.use(require('./api')(express));
-  router.use('/', express.static(path.join(appRoot, 'client/build')));
-  router.get('*', (req, res) => res.sendFile(path.join(appRoot, 'client/build/index.html')));
-  return router;
-};
